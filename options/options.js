c92fd84e7d023220c8139d9b07e1c4d43f13c4fb
Crowdbotics <team@crowdbotics.com>
2023-08-22 20:06:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sonic-kichei-43028",
+  url: "https://sonic-kichei-43028.botics.co",
+  api: "https://sonic-kichei-43028.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
